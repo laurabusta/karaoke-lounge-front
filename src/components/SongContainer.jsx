2fe67b7f761a3f21b4323fd267354b2456678c42
Laura Bustamante <laurabustamante@Lauras-2017-MacBook-Pro.local>
2021-04-02 12:13:36 -0400

    added a getSongs component and confirmed that it is fetching data from backend

--- src/components/SongContainer.jsx
+++ src/components/SongContainer.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+let baseURL = 'http://localhost:8000/api/v1/songs'
+
 class SongContainer extends React.Component {
+    state = {
+        songs: [],
+        testCount: 0
+    }
+
+    componentDidMount () {
+        this.getSongs()
+    }
+
+    getSongs() {
+        // this.setState({
+        //     testCount: this.state.testCount + 1
+        // })
+        fetch(baseURL + '/')
+            .then(data => {
+                return data.json()
+            }, err => console.log(err))
+            .then(parsedData => {
+                console.log(parsedData)
+                this.setState({
+                    songs: parsedData,
+                    testCount: this.state.testCount + 1
+                })
+            }, err => console.log(err))
+    }
+
     render () {
         return (
             <View>
                 <Text>SongContainer Component is here!</Text>
+                <Text>testCount = { this.state.testCount }</Text>
             </View>
         )
     }
