712b19802e79dec2d4a50c33a35bfcf6b9d0c37d
Laura Bustamante <laurabustamante@Lauras-2017-MacBook-Pro.local>
2021-04-02 11:39:42 -0400

    created intial app.js with class component structure in react native

--- App.js
+++ App.js
@@ -2,14 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+class App extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>Changed App.js to class component structure!</Text>
+      </View>
+    )
+  }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
